d39ab0b5482619218d7cb4d1f286f4ad3ecbcec5
Dziki00 <bartosz.dziki3@gmail.com>
2024-10-14 19:24:26 +0200

    Commit testowy

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.voteapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.voteapp to javafx.fxml;
+    exports com.example.voteapp;
+}
